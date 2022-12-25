46c061135076baca0788cd7bbc5d79f9495e95ad
tpltnt <1172976+tpltnt@users.noreply.github.com>
2022-12-25 21:49:46 +0100

    updated versioneer

--- pyutil/__init__.py
+++ pyutil/__init__.py
@@ -4,6 +4,5 @@
 Library of useful Python functions and classes.
 """
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
